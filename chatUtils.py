6b471b4c8f7a3f029066b73eeb2acfb44901f6c0
sumrender <sumrenders@gmail.com>
2023-03-06 10:51:06 +0530

    add index.json

--- chatUtils.py
+++ chatUtils.py
@@ -14,19 +14,19 @@ which product the user needs help with, in the first question only.
 """
 previous_questions_and_answers = []
 
+
 def ask_bot(new_question):
-    input_index = '/home/yzal/sambot_model/index.json'
+    input_index = './index.json'
     index = GPTSimpleVectorIndex.load_from_disk(input_index)
-    
+
     context = ""
     for question, answer in previous_questions_and_answers[-MAX_CONTEXT_QUESTIONS:]:
         context += QUESTION_SEQUENCE + question + ANSWER_SEQUENCE + answer
 
-    context += QUESTION_SEQUENCE + new_question + ANSWER_SEQUENCE    
+    context += QUESTION_SEQUENCE + new_question + ANSWER_SEQUENCE
     response = index.query(INSTRUCTIONS + context, response_mode="compact")
-    previous_questions_and_answers.append((new_question, response.response))  
+    previous_questions_and_answers.append((new_question, response.response))
     return response.response
- 
 
 
 if __name__ == "__main__":
